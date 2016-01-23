5e57988b0c7c8b2c089c15ae66350c4cc66f2c02
nitsanw <nitsanw@yahoo.com>
2016-01-23 17:01:08 +0200

    Variations on WhenWillItExit, bytecode and classes

---
+++ src/main/java/safepoint/hang/WhenWillItExitInt2.java
@@ -0,0 +1,23 @@
+package safepoint.hang;
+public class WhenWillItExitInt2 {
+  public static void main(String[] argc) throws InterruptedException {
+    Thread t = new Thread(() -> {
+      long l = countOdds(Integer.MAX_VALUE);
+      System.out.println("How Odd:" + l);
+    });
+    t.setDaemon(true);
+    t.start();
+    Thread.sleep(5000);
+  }
+
+  private static long countOdds(int limit) {
+    long l = 0;
+    for (int i = 0; i < limit; i++) {
+      for (int j = 0; j < limit; j++) {
+        if ((j & 1) == 1)
+          l++;
+      }
+    }
+    return l;
+  }
+}
